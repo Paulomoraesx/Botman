bfaaafb130f705c6e3449a077d31c814a7b4d3e4
Paulomoraesx <paulomoraesx1@gmail.com>
2020-10-21 19:02:19 -0300

    Carousel de teste na pagina de listagem de chatbots de acordo com o curso do usuario logado

--- resources/views/Chatbot/listarChatbotsCurso.blade.php
+++ resources/views/Chatbot/listarChatbotsCurso.blade.php
@@ -1,32 +1,62 @@
 @extends('template')
 @section('conteudo_principal')
 <div id="page" class="container">
-		<div class="title">
-			<h2>Chatbots</h2>
-		</div>
-		<table class="table table-hover">
-			    <thead>
-			      <tr>
-					<th>Nome</th>
-			        <th>Opções</th>
-			      </tr>
-			    </thead>
-			    <!-- DADOS -->
-			    <tbody>
-					@foreach ($chatbots as $chatbot)
-					<tr>
-						<td>{{$chatbot['titulo']}}</td>
-						<td>
-							<a href="{{route('chatbot.editar', ['id' => $chatbot['id']])}}" class="fa fa-pencil" style="text-decoration:none"></a>
-							<a href="{{route('chatbot.excluir',['id' => $chatbot['id']])}}" class="fa fa-trash" style="text-decoration:none"></a>
-                            <a href="{{route('chatbot.acessarChatBot',['id' => $chatbot['id']])}}" style="text-decoration:none">Acessar ChatBot</a>
-                            <a href="{{route('mensagem.cadastrar', ['id' => $chatbot['id']])}}" style="text-decoration:none">Gerenciar Fluxo de mensagens</a>
-						</td>
-					</tr>	
-					@endforeach
-			    </tbody>
-			    <!-- DADOS [FIM] -->
-			</table>
+    <div class="title">
+        <h2>Chatbots</h2>
+    </div>    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleControls" data-slide-to="1"></li>
+            <li data-target="#carouselExampleControls" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="testeCarossel">
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="testeCarossel2"><a>teste</a></div>
+            </div>
+            <div class="carousel-item">
+                <div class="testeCarossel3"><a>teste</a></div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Próximo</span>
+        </a>
+    </div>
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Opções</th>
+        </tr>
+        </thead>
+        <!-- DADOS -->
+        <tbody>
+        @foreach ($chatbots as $chatbot)
+        <tr>
+            <td>{{$chatbot['titulo']}}</td>
+            <td>
+                <a href="{{route('chatbot.editar', ['id' => $chatbot['id']])}}" class="fa fa-pencil"
+                   style="text-decoration:none"></a>
+                <a href="{{route('chatbot.excluir',['id' => $chatbot['id']])}}" class="fa fa-trash"
+                   style="text-decoration:none"></a>
+                <a href="{{route('chatbot.acessarChatBot',['id' => $chatbot['id']])}}" style="text-decoration:none">Acessar
+                    ChatBot</a>
+                <a href="{{route('mensagem.cadastrar', ['id' => $chatbot['id']])}}" style="text-decoration:none">Gerenciar
+                    Fluxo de mensagens</a>
+            </td>
+        </tr>
+        @endforeach
+        </tbody>
+        <!-- DADOS [FIM] -->
+    </table>
 </div>
 
 @endsection
